46160c58e53bc24cb43a2acb1d2fab4fd7379386
GuilhermeMViana <gmunizviana@gmail.com>
2025-10-13 22:12:04 -0300

    feat: appointment-form

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -9,21 +9,60 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
+import z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form } from '@/components/ui/form';
+
+const appointmentFormSchema = z.object({
+  tutorName: z.string().min(3, 'O nome do tutor precisa ser inserido'),
+  petName: z.string().min(3, 'O nome do pet precisa ser inserido'),
+  phone: z.string().min(11, 'O telefone deve ter DDD e número'),
+  description: z.string().min(3, 'Insira a descrição do serviço'),
+});
+
+type AppointmentFormValues = z.infer<typeof appointmentFormSchema>;
+
+const onSubmit = (data: AppointmentFormValues) => {
+  console.log(data);
+};
 
 export const AppointmentForm = () => {
+  const form = useForm<AppointmentFormValues>({
+    resolver: zodResolver(appointmentFormSchema),
+    defaultValues: {
+      tutorName: '',
+      petName: '',
+      phone: '',
+      description: '',
+    },
+  });
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="brand">Novo Agendamento</Button>
       </DialogTrigger>
 
-      <DialogContent>
+      <DialogContent
+        variant="appointment"
+        overlayVariant="blurred"
+        showCloseButton
+      >
         <DialogHeader>
-          <DialogTitle>Agende um atendimento</DialogTitle>
-          <DialogDescription>
+          <DialogTitle size="modal">Agende um atendimento</DialogTitle>
+          <DialogDescription size="modal">
             Preencha os dados do cliente para realizar o agendamento:
           </DialogDescription>
         </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <input {...form.register('tutorName')} type="text" />
+
+            <Button type="submit">Salvar</Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
